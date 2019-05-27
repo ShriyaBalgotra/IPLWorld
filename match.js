684296112e66257cd2367457d474b269d2d82214
Shriya Balgotra <30731005+ShriyaBalgotra@users.noreply.github.com>
2019-05-27 21:59:36 +0530

    Create match.js

---
+++ match.js
@@ -0,0 +1,423 @@
+var Match={
+  '2018-04-07':
+  [
+  {time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Mumbai Indians Vs Chennai Super Kings',
+  stadium:'Wankhede Stadium, Mumbai, Maharashtra'
+  }],
+  '2018-04-08':
+  [{time:'4 p m',
+  match:'Kings XI Punjab Vs Delhi Dare Devils',
+  stadium:'I.S.Bindra Stadium Mohali'
+  },
+  {time:'8 p m',
+  match:'Kolkata Knight Riders Vs Royal Challengers Bangalore',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+  }],
+  '2018-04-09':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Sun Risers Hyderabad Vs Rajasthan Royals',
+  stadium:'Rajiv Gandhi International Stadium, Hyderabad'
+  }],
+  '2018-04-10':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Chennai Super Kings Vs Kolkata Knight Riders',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  }],
+  '2018-04-11':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Rajasthan Royals Vs Delhi Dare Devils',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+  }],
+  '2018-04-12':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Sunrisers Hyderabad Vs Mumbai Indians',
+  stadium:'Rajiv Gandhi International Stadium, Hyderabad'
+  }],
+  '2018-04-13':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Royal Challengers Bangalore Vs Kings XI Punjab',
+  stadium:'M.Chinnaswamy Stadium Bengaluru, Karnataka'
+  }],
+  '2018-04-14':
+  [{time:'4 p m',
+  match:'Mumbai Indians Vs Delhi Dare Devils',
+  stadium:'Wankhede Stadium Mumbai, Maharashtra'
+  },
+  {time:'8 p m',
+  match:'Kolkata Knight Riders Vs Sun Risers Hyderabad',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+  }],
+  '2018-04-15':
+  [{time:'4 p m',
+  match:'Royal Challengers Bangalore Vs Rajasthan Royals',
+  stadium:'M.Chinnaswamy Stadium Bengaluru, Karnataka'
+  },
+  {time:'8 p m',
+  match:'Kings XI Punjab Vs Chennai Supers Kings',
+  stadium:'I.S.Bindra Stadium, Mohali'
+  }],
+  '2018-04-16':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Kolkata Knight Riders Vs Delhi Dare Devils',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+  }],
+  '2018-04-17':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Mumbai Indians Vs Royal Challengers Bangalore',
+  stadium:'Wankhede Stadium Mumbai, Maharashtra'
+  }],
+  '2018-04-18':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Rajasthan Royals Vs Kolkata Knight Riders',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+  }],
+  '2018-04-19':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Kings XI Punjab Vs Sun Risers Hyderabad',
+  stadium:'I.S.Bindra Stadium, Mohali'
+  }],
+  '2018-04-20':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Chennai Supers Kings Vs Rajasthan Royals',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  }],
+  '2018-04-21':
+  [{time:'4 p m',
+  match:'Kolkata Knight Riders Vs Kings XI Punjab',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+  },
+  {time:'8 p m',
+  match:'Delhi Dare Devils Vs Royal Challengers Bangalore',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+  }],
+  '2018-04-22':
+  [{time:'4 p m',
+  match:'Sunrisers Hyderabad Vs Chennai Super Kings',
+  stadium:'Rajiv Gandhi International Stadium, Hyderabad'
+  },
+  {time:'8 p m',
+  match:'Rajasthan Royals Vs Mumbai Indians',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+  }],
+  '2018-04-23':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Delhi Dare Devils vs Kings XI Punjab',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+  }],
+  '2018-04-24':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Mumbai Indians Vs Sunrisers Hyderabad',
+  stadium:'Wankhede Stadium Mumbai, Maharashtra'
+  }],
+  '2018-04-25':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Royal Challengers Bangalore Vs Chennai Super Kings',
+  stadium:'M.Chinnaswamy Stadium Bengaluru, Karnataka'
+  }],
+  '2018-04-26':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Sunrisers Hyderabad Vs Kings XI Punjab',
+  stadium:'Rajiv Gandhi International Stadium, Hyderabad'
+  }],
+  '2018-04-27':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Delhi Dare Devils Vs Kolkata Knight Riders',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+  }],
+  '2018-04-28':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Chennai Super Kings Vs Mumbai',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  }],
+  '2018-04-29':
+  [{time:'4 p m',
+  match:'Rajasthan Royals vs Sunrisers Hyderabad',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+  },
+  {time:'8 p m',
+  match:'RCB vs Kolkata Knight Riders',
+  stadium:'M.Chinnaswamy Stadium Bengaluru, Karnataka'
+  }],
+  '2018-04-30':
+  [{time:'4 p m',
+  match:'no match',
+  },
+  {time:'8 p m',
+  match:'Chennai Super Kings Vs Delhi Dare Devils',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  }],
+  '2018-05-01':
+  [{time:'4 p m',
+  match:'no another match',
+  },
+    {time:'8 p m',
+  match:'Royal Challengers Bangalore Vs Mumbai Indians',
+  stadium:'M.Chinnaswamy Stadium Bengaluru, Karnataka'
+}],
+  '2018-05-02':
+  [{time:'4 p m',
+    match:'no another match'
+  },
+    {time:'8 p m',
+  match:'Delhi Dare Devils vs Rajasthan Royals',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+}],
+  '2018-05-03':
+  [{time:'4 p m',
+    match:'no another match'
+  },
+  {time:'8 p m',
+  match:'	Kolkata Knight Riders vs Chennai Super Kings',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+}],
+  '2018-05-04':
+  [{time:'4 p m',
+    match:'no another match'
+  },
+    {time:'8 p m',
+  match:'Kings XI Punjab vs Mumbai Indians',
+  stadium:'Holkar Stadium, Indore, Madhya Pradesh'
+}],
+  '2018-05-05':
+  [{time:'4 p m',
+  match:'Chennai Super Kings Vs Royal Challengers Bangalore',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  },
+  {time:'8 p m',
+  match:'Sunrisers Hyderabad vs Delhi Dare Devils',
+  stadium:'Rajiv Gandhi International Stadium, Hyderabad'
+}],
+  '2018-05-06':
+  [{time:'4 p m',
+  match:'Mumbai Indians vs Kolkata Knight Riders',
+  stadium:'	Wankhede Stadium Mumbai, Maharashtra'
+},
+  {time:'8 p m',
+  match:'Kings XI Punjab vs Rajasthan Royals',
+  stadium:'Holkar Stadium, Indore, Madhya Pradesh'
+  }],
+  '2018-05-07':
+  [{time:'4 p m',
+  match:'no match'
+},
+  {time:'8 p m',
+  match:'Sunrisers Hyderabad Vs Royal Challengers Bangalore',
+  stadium:'Rajiv Gandhi International Stadium, Hyderabad'
+  }],
+  '2018-05-08':
+  [{time:'4 p m',
+  match:'no match'
+  },
+    {time:'8 p m',
+  match:'Rajasthan Royals vs Kings XI Punjab',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+}],
+  '2018-05-09':
+  [{time:'4 p m',
+  match:'no match'
+},
+  {time:'8 p m',
+  match:'Kolkata Knight Riders vs Mumbai Indians',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+  }],
+  '2018-05-10':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Delhi Dare Devils Vs Sunrisers Hyderabad',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+  }],
+  '2018-05-11':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Rajasthan Royals vs Chennai Super Kings',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+  }],
+  '2018-05-12':
+  [{time:'4 p m',
+  match:'Kings XI Punjab Vs Kolkata Knight Riders',
+  stadium:'Holkar Stadium, Indore, Madhya Pradesh'
+},
+  {time:'8 p m',
+  match:'Royal Challengers Bangalore Vs Delhi Dare Devils',
+  stadium:'M.Chinnaswamy Stadium Bengaluru, Karnataka'
+  }],
+  '2018-05-13':
+  [{time:'4 p m',
+  match:'Chennai Super Kings Vs Sunrisers Hyderabad',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  },
+  {time:'8 p m',
+  match:'Mumbai Indians Vs Rajasthan Royals',
+  stadium:'Wankhede Stadium Mumbai, Maharashtra'
+  }],
+  '2018-05-14':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Kings XI Punjab Vs Royal Challengers Bangalore',
+  stadium:'Holkar Stadium, Indore, Madhya Pradesh'
+  }],
+  '2018-05-15':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Kings XI Punjab Vs Royal Challengers Bangalore',
+  stadium:'Holkar Stadium, Indore, Madhya Pradesh'
+  }],
+  '2018-05-16':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Kolkata Knight Riders Vs Rajasthan Royals',
+  stadium:'Eden Garden Stadium, Kolkata, West Bengal'
+  }],
+  '2018-05-17':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Royal Challengers Bangalore Vs Sunrisers Hyderabad',
+  stadium:'M.Chinnaswamy Stadium Bengaluru'
+  }],
+  '2018-05-18':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Delhi Dare Devils Vs Chennai Super Kings',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+  }],
+  '2018-05-19':
+  [{time:'4 p m',
+  match:'Rajasthan Royals Vs Royal Challengers Bangalore',
+  stadium:'Sawai Man Singh Stadium, Jaipur, Rajasthan'
+  },
+  {time:'8 p m',
+  match:'Sunrisers Hyderabad Vs Kolkata Knight Riders',
+  stadium:'	Rajiv Gandhi International Stadium, Hyderabad'
+  }],
+  '2018-05-20':
+  [{time:'4 p m',
+  match:'Delhi Dare Devils vs Mumbai Indians',
+  stadium:'Feroz Shah Kotla Stadium, Delhi'
+  },
+  {time:'8 p m',
+  match:'Chennai Super Kings Vs Kings XI Punjab',
+  stadium:'M.A.Chidambaram Stadium Chennai, Tamil Nadu'
+  }],
+  '2018-05-21':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'no match'
+  }],
+  '2018-05-22':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Qualifier 1',
+  stadium:'Wankhede Stadium Mumbai, Maharashtra'
+  }],
+  '2018-05-23':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:' Eliminator',
+  stadium:'	TBC'
+  }],
+  '2018-05-24':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'no match',
+  }],
+  '2018-05-25':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+    match:'Qualifier 2 {IPL 2018 Match}',
+    stadium:'	TBC'
+  }],
+  '2018-05-26':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'no match',
+  }],
+  '2018-05-27':
+  [{time:'4 p m',
+  match:'no match'
+  },
+  {time:'8 p m',
+  match:'Final VIVO IPL 2018 Match',
+  stadium:'Wankhede Stadium Mumbai, Maharashtra'
+  }],
+};
+module.exports = Match;
+
+
